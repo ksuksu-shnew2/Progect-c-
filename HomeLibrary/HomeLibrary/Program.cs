d385393bd4a8dc45cb12a28d3200ace5e3fcf916
ksuksu-shnew2 <sh_ksenya@mail.ru>
2026-02-12 22:42:39 +0300

    dz-module-5

--- HomeLibrary/HomeLibrary/Program.cs
+++ HomeLibrary/HomeLibrary/Program.cs
@@ -15,20 +15,71 @@ using System;
 // Console.WriteLine("Год издания: "+ Year);
 // Console.WriteLine("ISBN: "+ ISBN);
 
-Console.WriteLine("Введите значение: ");
-int value = int.Parse(Console.ReadLine()); 
+// Console.WriteLine("Введите значение: ");
+// int value = int.Parse(Console.ReadLine()); 
 
 
-switch (value)
-{
-case 1:
-Console.WriteLine("Добавить книгу");
-break;
-case 2:
-Console.WriteLine("Показыть книгу");
-break;
-default:
-Console.WriteLine("Выйти");
-break;
-}
+// switch (value)
+// {
+// case 1:
+// Console.WriteLine("Добавить книгу");
+// break;
+// case 2:
+// Console.WriteLine("Показыть книгу");
+// break;
+// default:
+// Console.WriteLine("Выйти");
+// break;
+// }
+
+//Console.WriteLine("Введите значение: ");
+
+string[,] matrix = new string[5,4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+
+        {
+            Console.WriteLine($"Книга №{i+1}");
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        string text;
+                        switch (j)
+                        {
+                        case 0:
+                        text = "Введите название книги";
+                        break;
+                        case 1:
+                        text = "Введите автора книги:";
+                        break;
+                        case 2:
+                        text = "Введите год издания книги: ";
+                        break;
+                        case 3:
+                        text = "Введите ISBN";
+                        break;
+                        default:
+                        text ="Выйти";
+                        break;
+                        }
+
+                        Console.Write($"{text}: ");
+                      
+                            matrix[i, j] = Console.ReadLine();                    
+
+                    }
+        }
+
+        Console.WriteLine("\nМассив получился такой:");
+
+        // Вывод
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
 
