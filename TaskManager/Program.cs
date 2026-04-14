0b8feef218b16c9c964879eb356fc07ce09f44d3
kpshmyk <sh_ksenya@mail.ru>
2026-04-14 21:20:42 +0300

    е

---
+++ TaskManager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TaskManager;
+
+class Program
+{
+
+    static void Main()
+    {
+    
+    }
+    internal static void PrintMenu()
+    {
+        Console.WriteLine("\n=== Менеджер задач ===");
+        Console.WriteLine("1. Показать все задачи");
+        Console.WriteLine("2. Добавить задачу");
+        Console.WriteLine("3. Удалить задачу");
+        Console.WriteLine("0. Выход\n");
+        Console.WriteLine("Ваш выбор: ");
+    }
+}
+
+
