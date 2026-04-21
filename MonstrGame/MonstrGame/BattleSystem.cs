fb3bf0b152e7b770d6d6450fa56bd596121c092c
kpshmyk <sh_ksenya@mail.ru>
2026-04-21 23:18:03 +0300

    game

--- MonstrGame/MonstrGame/BattleSystem.cs
+++ MonstrGame/MonstrGame/BattleSystem.cs
@@ -9,7 +9,6 @@ public class BattleSystem
 
     public Random rnd = new();
 
-     
 
     public void StartBattle()
     {
@@ -33,11 +32,9 @@ public class BattleSystem
                 int damage = rnd.Next(10, 21);
 
                 int crit = rnd.Next(0, 100);
-                //monster.Health =
+             
                 TakeDamagePlayer(damage, crit);
-                //int damage = player.damage;//rnd.Next(10, 21);
-
-                //int crit = player.crit;//rnd.Next(0, 100);
+             
 
             }
          else if (numberMenu == 2)
@@ -45,18 +42,15 @@ public class BattleSystem
                 int miss = rnd.Next(0, 100);
                 int crit = rnd.Next(0, 100);
                 int damageHigh = rnd.Next(20, 36);
-                //monster.Health =
+                
                 TakeDamagePlayerHigh(damageHigh, crit, miss);
-                //int miss = rnd.Next(0, 100);
+                
             }
         else if (numberMenu == 3)
         {
-                //player.Health = Health(player.countHelf, monster.Health, player.Health, player.helf);
+               
                 player.Half();
-                //player.Health = MinusNull(player.Health);
-            
-                //Console.WriteLine($"\nТы восстановил {player.helf} здоровья. У тебя осталось {player.Health} здоровья,а у монстра осталось {monster.Health} здоровья.");
-
+                
             }
         else if (numberMenu == 0) 
         {
@@ -75,7 +69,7 @@ public class BattleSystem
                 int damage = rnd.Next(12, 23);
                 int critMonstr = rnd.Next(0, 100);
                 int evasion = rnd.Next(0, 100);
-                //player.Health =
+               
                 TakeDamageMonster(damage, critMonstr,  evasion);
             }
 
@@ -90,7 +84,6 @@ public class BattleSystem
         }
     }
 
-                //int miss = rnd.Next(0, 100);
 
 
     internal static void PrintMenu()
@@ -110,24 +103,16 @@ public class BattleSystem
                 {
                     damage *= 2;
                     monster.TakeDamage(damage);
-                //  MinusNull(monster.Health);
-            //     if (monster.Health < 0)
-            // {
-            //     monster.Health = 0;
-            // }
+              
                     Console.WriteLine($"\nКритический удар! Ты нанес монстру {damage} урона. У монстра осталось {monster.Health} здоровья.");
                 }
                 else
                 {
                     monster.TakeDamage(damage);
-                    //MinusNull(monster.Health);
-            // if (monster.Health < 0)
-            //     {
-            //         monster.Health = 0;
-            //     }
+          
                     Console.WriteLine($"\nТы нанес монстру {damage} урона. У монстра осталось {monster.Health} здоровья.");
         }
-       // return Health;
+       
 
     }
     internal  void TakeDamageMonster(int damage, int critMonstr, int evasion)
@@ -146,10 +131,10 @@ public class BattleSystem
                 else
                 {
                 player.TakeDamage(damage);
-                 //MinusNull(player.Health);
+                 
                 Console.WriteLine($"\nМонстр атакует тебя и наносит {damage} урона. У тебя осталось {player.Health} здоровья.");
         }
-    //return Health;
+   
 
     }
     internal  void TakeDamagePlayerHigh(int damageHigh, int crit,int miss)
@@ -161,57 +146,23 @@ public class BattleSystem
                 if(crit<15)
                 {
                     damageHigh *= 2;
-                //Health -= damageHigh;
+               
                 monster.TakeDamage(damageHigh);
-                //MinusNull(monster.Health);
+               
                         Console.WriteLine($"\nКритический удар! Ты нанес монстру {damageHigh} урона. У монстра осталось {monster.Health} здоровья.");
                 }
                 else
                     {
                 monster.TakeDamage(damageHigh);
-               //MinusNull(monster.Health);
+               
                Console.WriteLine($"\nТы нанес монстру {damageHigh} урона. У монстра осталось {monster.Health} здоровья.");
 
 
-            }   //Console.WriteLine($"\nТы нанес монстру {damageHigh} урона. У монстра осталось {Health} здоровья.");
+            }   
         }
-     //   return Health;
+   
 
     }
-
-    // internal static int Health(int countHelf, int Health,int Health,int helf)
-    // {
-    //        if (countHelf<= 0)
-    //             {
-    //                 Console.WriteLine($"\nУ тебя закончились зелья здоровья! Выбери другое действие.");
-    //             }
-    //             else if (Health == 100)
-    //             {
-    //                 Console.WriteLine($"\nУ тебя полное здоровье! Выбери другое действие.");
-    //             }
-    //             else
-    //             {
-    //                 Health += helf;
-    //                 countHelf--;
-    //                 Console.WriteLine($"\nТы использовал зелье здоровья. Осталось: {countHelf}");
-    //             }
-    //             Health = MinusNull(Health);
-            
-    //             Console.WriteLine($"\nТы восстановил {helf} здоровья. У тебя осталось {Health} здоровья,а у монстра осталось {Health} здоровья.");
-
-    //             return Health;
-
-    // }
-
-
-    // static int MinusNull(int helf)
-    // {
-    //     if (helf < 0)
-    //     {
-    //         helf = 0;
-    //     }
-    //     return helf;
-    // }
 
     static int ReadInt()
     {
