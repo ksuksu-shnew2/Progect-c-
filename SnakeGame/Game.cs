5f32a68b452fc9af9cd10365fe84396602f714c8
kpshmyk <sh_ksenya@mail.ru>
2026-04-26 19:22:55 +0300

    game

--- SnakeGame/Game.cs
+++ SnakeGame/Game.cs
@@ -3,117 +3,148 @@ namespace SnakeGame;
 public class Game
 {
     Snake snake = new Snake();
-    Food food = new Food(5,5);
+    Food food = new Food(5, 5);
 
     int width = 20;
     int height = 10;
-
-   public void Start()
-{
     int score = 0;
 
-    while (true)
+    public void Start()
     {
-        if (Console.KeyAvailable)
+        while (true)
         {
-            ConsoleKey key = Console.ReadKey(true).Key;
+            ReadInput();
 
-            if (key == ConsoleKey.W)
-                snake.Direction = Direction.Up;
-            else if (key == ConsoleKey.S)
-                snake.Direction = Direction.Down;
-            else if (key == ConsoleKey.A)
-                snake.Direction = Direction.Left;
-            else if (key == ConsoleKey.D)
-                snake.Direction = Direction.Right;
-        }
+            Position currentHead = snake.Body[0];
+            Position nextHead = new Position(currentHead.X, currentHead.Y);
 
-        snake.Move();
+            if (snake.Direction == Direction.Right) nextHead.X++;
+            else if (snake.Direction == Direction.Left) nextHead.X--;
+            else if (snake.Direction == Direction.Up) nextHead.Y--;
+            else if (snake.Direction == Direction.Down) nextHead.Y++;
 
-        Position head = snake.Body[0];
+            bool willEat = nextHead.X == food.Pos.X && nextHead.Y == food.Pos.Y;
 
-        if (head.X == 0 || head.X == width - 1 || head.Y == 0 || head.Y == height - 1)
-        {
-            Console.Clear();
-            Console.WriteLine("GAME OVER");
-            Console.WriteLine($"Score: {score}");
-            return;
-        }
+            snake.Move(willEat);
 
-        foreach (var part in snake.Body.Skip(1))
-        {
-            if (head.X == part.X && head.Y == part.Y)
+            Position head = snake.Body[0];
+
+            if (IsWallCollision(head) || IsSelfCollision(head))
             {
                 Console.Clear();
                 Console.WriteLine("GAME OVER");
                 Console.WriteLine($"Score: {score}");
                 return;
             }
+
+            if (willEat)
+            {
+                score++;
+                SpawnFood();
+            }
+
+            Draw();
+            Console.WriteLine($"Score: {score}");
+
+            Thread.Sleep(800);
         }
-
-        if (head.X == food.Pos.X && head.Y == food.Pos.Y)
-        {
-            snake.Grow();
-            SpawnFood();
-            score++;
-        }
-
-        Draw();
-        Console.WriteLine($"Score: {score}");
-
-        Thread.Sleep(500);
     }
-}
-    
+
+    void ReadInput()
+    {
+        if (!Console.KeyAvailable)
+            return;
+
+        ConsoleKey key = Console.ReadKey(true).Key;
+
+        if ((key == ConsoleKey.W || key == ConsoleKey.UpArrow) && snake.Direction != Direction.Down)
+    snake.Direction = Direction.Up;
+
+else if ((key == ConsoleKey.S || key == ConsoleKey.DownArrow) && snake.Direction != Direction.Up)
+    snake.Direction = Direction.Down;
+
+else if ((key == ConsoleKey.A || key == ConsoleKey.LeftArrow) && snake.Direction != Direction.Right)
+    snake.Direction = Direction.Left;
+
+else if ((key == ConsoleKey.D || key == ConsoleKey.RightArrow) && snake.Direction != Direction.Left)
+    snake.Direction = Direction.Right;
+    }
+
+    bool IsWallCollision(Position head)
+    {
+        return head.X == 0 ||
+               head.X == width - 1 ||
+               head.Y == 0 ||
+               head.Y == height - 1;
+    }
+
+    bool IsSelfCollision(Position head)
+    {
+        foreach (var part in snake.Body.Skip(1))
+        {
+            if (head.X == part.X && head.Y == part.Y)
+                return true;
+        }
+
+        return false;
+    }
+
     void Draw()
     {
-    Console.Clear();
+        Console.Clear();
 
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
         {
-            bool isSnake = false;
+            for (int x = 0; x < width; x++)
+            {
+                bool isSnake = false;
+
+                foreach (var part in snake.Body)
+                {
+                    if (part.X == x && part.Y == y)
+                    {
+                        isSnake = true;
+                        break;
+                    }
+                }
+
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                    Console.Write("#");
+                else if (x == food.Pos.X && y == food.Pos.Y)
+                    Console.Write("*");
+                else if (isSnake)
+                    Console.Write("O");
+                else
+                    Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    void SpawnFood()
+    {
+        while (true)
+        {
+            int x = Random.Shared.Next(1, width - 1);
+            int y = Random.Shared.Next(1, height - 1);
+
+            bool insideSnake = false;
 
             foreach (var part in snake.Body)
             {
                 if (part.X == x && part.Y == y)
                 {
-                    isSnake = true;
+                    insideSnake = true;
                     break;
                 }
             }
 
-            if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+            if (!insideSnake)
             {
-                Console.Write("#");
-            }
-            else if (x == food.Pos.X && y == food.Pos.Y)
-            {
-                Console.Write("*");
-            }
-            else if (isSnake)
-            {
-                Console.Write("O");
-            }
-            else
-            {
-                Console.Write(" ");
+                food = new Food(x, y);
+                return;
             }
         }
-
-        Console.WriteLine();
     }
-}
-
- void SpawnFood()
-    {
-        int x = Random.Shared.Next(1, width - 1);
-        int y = Random.Shared.Next(1, height - 1);
-
-        food = new Food(x, y);
-    }
-    
-
-    
 }
--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -11,30 +11,22 @@ public class Snake
         Body.Add(new Position(9, 5));
         Body.Add(new Position(8, 5));
     }
-        public void Move()
+
+    public void Move(bool grow)
+    {
+        Position head = Body[0];
+        Position newHead = new Position(head.X, head.Y);
+
+        if (Direction == Direction.Right) newHead.X++;
+        else if (Direction == Direction.Left) newHead.X--;
+        else if (Direction == Direction.Up) newHead.Y--;
+        else if (Direction == Direction.Down) newHead.Y++;
+
+        Body.Insert(0, newHead);
+
+        if (!grow)
         {
-            Position head = Body[0];
-            Position newHead = new Position(head.X, head.Y);
-
-            if (Direction == Direction.Right)
-                newHead.X++;
-
-            if (Direction == Direction.Left)
-                newHead.X--;
-
-            if (Direction == Direction.Up)
-                newHead.Y--;
-
-            if (Direction == Direction.Down)
-                newHead.Y++;
-
-            Body.Insert(0, newHead);
             Body.RemoveAt(Body.Count - 1);
         }
-
-          public void Grow()
-        {
-            Position tail = Body[Body.Count - 1];
-            Body.Add(new Position(tail.X, tail.Y));
-        }
+    }
 }
