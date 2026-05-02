411a51ad43757f5f82ca0a0a8c2911952fdb5b6f
kpshmyk <sh_ksenya@mail.ru>
2026-05-02 21:28:47 +0300

    game car

--- GtaCar/Car.cs
+++ GtaCar/Car.cs
@@ -2,5 +2,20 @@ namespace GtaCar;
 
 public class Car
 {
-    
+    public Position Pos;
+    public bool IsStolen = false;
+
+    public Car(int x, int y) 
+    {
+        Pos = new Position(x, y);
+    }
+
+    public void TrySteal(Player player)
+    {
+        if (player.Pos.X == Pos.X && player.Pos.Y == Pos.Y && !IsStolen)
+        {
+            IsStolen = true;
+            player.TakeCar();
+        }
+    }
 }
--- GtaCar/Garage.cs
+++ GtaCar/Garage.cs
@@ -2,5 +2,18 @@ namespace GtaCar;
 
 public class Garage
 {
-    
+    public Position Pos;
+
+    public Garage(int x, int y) 
+    {
+        Pos = new Position(x, y);
+    }
+
+    public void TryAccept(Player player)
+    {
+        if (player.Pos.X == Pos.X && player.Pos.Y == Pos.Y && player.HasCar)
+        {
+            player.DeliverCar();
+        }
+    }
 }
